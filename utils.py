f3c8ae1ea50a91fc3b0ef2cbf75c9cf678ee81be
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-03-30 13:23:14 +0200

    add rules to DataFrame

--- utils.py
+++ utils.py
@@ -1,3 +1,35 @@
+from pandas import DataFrame
+import json
 
-# just testing
-x = 3
+IMPLICIT_TRANSACTIONS_CONDITION = {
+    "op": "and",
+    "inner": []
+}
+
+
+def json_rules_to_frame(file_name: str) -> DataFrame:
+    with open(file_name, "rt") as f:
+        rules = json.load(f)
+    simple_rules = []
+    for rule in rules:
+        rule_id = rule["id"]
+        if rule["condition"] == IMPLICIT_TRANSACTIONS_CONDITION:
+            name = "*"
+        else:
+            name = rule["condition"]["inner"][0]["value"]
+
+        if rule["type"] == "transaction" and 1400 <= rule_id < 1500:
+            simple_rules.append({
+                "factor": rule["samplingValue"]["value"],
+                "name": name
+            })
+
+    ret_val = DataFrame(data=simple_rules)
+    return ret_val
+
+
+
+# for debug
+if __name__ == '__main__':
+    pd = json_rules_to_frame("rules-2023-03-30.json")
+    print(pd)
